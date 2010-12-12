ffbcc575965e09bfa4f3b82a47d7d6a3e9396198
Derek Williams <derek@nebvin.ca>
2010-12-11 17:25:22 -0700

    clean up tests

--- src/test/scala/FuturesSpec.scala
+++ src/test/scala/FuturesSpec.scala
@@ -14,7 +14,7 @@ class AkkaFuturesSpec extends Specification {
 
   "akka futures" should {
     "have scalaz functor instance" in {
-      val f1 = future(5000)({Thread.sleep(1000);5 * 5})
+      val f1 = future(5000)(5 * 5)
       val f2 = f1 ∘ (_ * 2)
       val f3 = f2 ∘ (_ * 10)
       val f4 = f1 ∘ (_ / 0)
@@ -26,7 +26,7 @@ class AkkaFuturesSpec extends Specification {
       f5.await.resultOrException must throwA(new ArithmeticException("/ by zero"))
     }
     "have scalaz bind instance" in {
-      val f1 = future(5000)({Thread.sleep(1000);5 * 5})
+      val f1 = future(5000)(5 * 5)
       val f2 = f1 flatMap (x => future(5000)(x * 2))
       val f3 = f2 flatMap (x => future(5000)(x * 10))
       val f4 = f1 flatMap (x => future(5000)(x / 0))
@@ -38,24 +38,25 @@ class AkkaFuturesSpec extends Specification {
       f5.await.resultOrException must throwA(new ArithmeticException("/ by zero"))
     }
     "have scalaz apply instance" in {
-      val f1 = future(5000)({Thread.sleep(1000);5 * 5})
+      val f1 = future(5000)(5 * 5)
       val f2 = f1 ∘ (_ * 2)
       val f3 = f2 ∘ (_ / 0)
 
       (f1 |@| f2)(_ * _).await.resultOrException must_== Some(1250)
       (f1 |@| f2).tupled.await.resultOrException must_== Some((25,50))
       (f1 |@| f2 |@| f3)(_ * _ * _).await.resultOrException must throwA(new ArithmeticException("/ by zero"))
+      (f3 |@| f2 |@| f1)(_ * _ * _).await.resultOrException must throwA(new ArithmeticException("/ by zero"))
     }
     "calculate fib seq" in {
       def seqFib(n: Int): Int = if (n < 2) n else seqFib(n - 1) + seqFib(n - 2)
 
       def fib(n: Int): Future[Int] =
         if (n < 30)
-          future(5000)(seqFib(n))
+          future(0)(seqFib(n))
         else
           fib(n - 1).<**>(fib(n - 2))(_ + _)
 
-      fib(46).await.result must_== Some(1836311903)
+      fib(46).awaitBlocking.result must_== Some(1836311903)
     }
   }
 }
