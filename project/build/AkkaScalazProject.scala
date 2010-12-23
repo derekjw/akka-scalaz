bfa3a52417f9d6f4114ef1d850c4040d27d4913c
Derek Williams <derek@nebvin.ca>
2010-12-23 12:45:00 -0700

    Publish to repo

--- project/build/AkkaScalazProject.scala
+++ project/build/AkkaScalazProject.scala
@@ -7,5 +7,18 @@ class AkkaScalazProject(info: ProjectInfo) extends DefaultProject(info) with Akk
   val scalazCore = "com.googlecode.scalaz" %% "scalaz-core" % "5.1-SNAPSHOT"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
 
+  override def managedStyle = ManagedStyle.Maven
+  val publishUser = "derek"
+  val publishKeyFile = new java.io.File("/home/derek/.ssh/id_rsa")
+  val publishTo = projectVersion.value match {
+    case BasicVersion(_,_,_,Some("SNAPSHOT")) =>
+      Resolver.sftp("Fyrie Snapshots SFTP", "repo.fyrie.net", "/home/repo/snapshots") as(publishUser, publishKeyFile)
+    case _ =>
+      Resolver.sftp("Fyrie Releases SFTP", "repo.fyrie.net", "/home/repo/releases") as(publishUser, publishKeyFile)
+  }
+
+  val fyrieReleases           = "Fyrie releases" at "http://repo.fyrie.net/releases"
+  val fyrieSnapshots          = "Fyrie snapshots" at "http://repo.fyrie.net/snapshots"
+
   val scalaToolsSnapshots     = ScalaToolsSnapshots
 }
