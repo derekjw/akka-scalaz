8a818e42ee8ee3d91718f4886e62dc8ec1f32d4f
Derek Williams <derek@nebvin.ca>
2011-01-07 12:16:49 -0700

    Update to akka 1.0-rc3

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,5 +2,5 @@ import sbt._
  
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val akkaRepo   = "Akka Repository" at "http://scalablesolutions.se/akka/repository"
-  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0-RC2"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0-RC3"
 }
